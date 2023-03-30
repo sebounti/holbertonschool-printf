54bd5e28dc79c9588bc987a41169b99702749516
Lenne Sebastien <sebastienlenne@gmail.com>
2023-03-30 15:53:49 +0200

    test2

--- _printf.c
+++ _printf.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 /**
  * _printf - print function
@@ -10,44 +11,50 @@
  **/
 int _printf(const char *format, ...)
 {
-	int i;
-	char *str = "";
-	char str_len;
-	va_list arg_list;
-	int char_count = 0;
+    int i;
+    char *str = "";
+    char str_len;
+    va_list arg_list;
+    int char_count = 0;
 
-	va_start(arg_list, format);
-		while ((*format) != '\0')
-		{
-			if (*format != '%')
-				{
-				putchar(*format);
-				char_count++;
-				}
-			else
-			{
-				format++;
-				if (*format == 'c')
-					{
-					putchar(va_arg(arg_list, int));
-					char_count++;
-					}
-				else if (*format == '%')
-					{
-					putchar('%');
-					char_count++;
-					}
-				else if (*format == 's')
-				str = va_arg(arg_list, char*);
-				str_len = strlen(str);
-				for (i = 0; i < str_len; i++)
-					{
-					putchar(str[i]);
-					char_count++;
-					}
-			}
-		format++;
-		}
-	va_end(arg_list);
-	return (char_count);
+ if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+        return (-1);
+
+    va_start(arg_list, format);
+        if (format == 0)
+            return (0);
+
+        while ((*format) != '\0')
+        {
+            if (*format != '%')
+                {
+                putchar(*format);
+                char_count++;
+                }
+            else
+            {
+                format++;
+                if (*format == 'c')
+                    {
+                    putchar(va_arg(arg_list, int));
+                    char_count++;
+                    }
+                else if (*format == '%')
+                    {
+                    putchar('%');
+                    char_count++;
+                    }
+                else if (*format == 's')
+                str = va_arg(arg_list, char*);
+                str_len = strlen(str);
+                for (i = 0; i < str_len; i++)
+                    {
+                    putchar(str[i]);
+                    char_count++;
+                    }
+            }
+        format++;
+        }
+    va_end(arg_list);
+    return (char_count);
 }
